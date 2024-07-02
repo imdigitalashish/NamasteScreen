9a416dd6d4fc3953ba5f7fe2b6aa5f72ef89a18a
Ashish kumar Verma <imdigitalashish@gmail.com>
2024-07-02 20:29:56 +0530

    Added Time Effect

--- app/src/main/java/com/example/androidlauncher/MainActivity.kt
+++ app/src/main/java/com/example/androidlauncher/MainActivity.kt
@@ -6,11 +6,13 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -18,10 +20,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidlauncher.ui.theme.AndroidLauncherTheme
+import kotlinx.coroutines.delay
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,9 +75,18 @@ fun AppList(context: Context) {
         }
     }
 
+
+
     // Display the list of apps
 
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+    Column(
+        modifier = Modifier
+            .background(Color.Black)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        CurrentTimeDisplay()
+
         apps.forEach { app ->
             Button(onClick = {
                 val launchIntent = Intent(Intent.ACTION_MAIN)
@@ -84,6 +105,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+    )
+}
+
+@Composable
+fun CurrentTimeDisplay() {
+    var currentTime by remember { mutableStateOf("") }
+
+    LaunchedEffect(key1 = Unit) {
+        while (true) {
+            val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
+            currentTime = sdf.format(Date())
+            delay(1000) // Update every second
+        }
+    }
+
+    Text(
+        text = "Current time: $currentTime",
+        color = Color.White,
+        modifier = Modifier.padding(16.dp)
     )
 }
 
